b11389de980b1e386affe67ed3af0bd604f2cc80
Miguel M <magonzalez@fortinet.com>
2019-11-20 17:18:52 +0100

    WIP: More requests added

--- fortimanager_sdwan.py
+++ fortimanager_sdwan.py
@@ -44,10 +44,10 @@ headers = {
 
 response = requests.request("POST", url, data=json.dumps(payload), headers=headers, verify=False)
 
+# TODO: get session only on succesful login
+
 content = json.loads(response.content)
 print(content)
-
-# TODO: get session only on succesful login
 session = content["session"]
 
 
@@ -81,7 +81,12 @@ headers = {
 
 response = requests.request("POST", url, data=json.dumps(payload), headers=headers, verify=False)
 
-print(response.text)
+# TODO: exit if not on succesful login
+
+content = json.loads(response.content)
+sn_fgt_1 = content["result"][0]["data"]["device"]["sn"]
+
+print(sn_fgt_1)
 
 ##############################################################
 # 2
@@ -113,7 +118,12 @@ headers = {
 
 response = requests.request("POST", url, data=json.dumps(payload), headers=headers, verify=False)
 
-print(response.text)
+# TODO: exit if not on succesful login
+
+content = json.loads(response.content)
+sn_fgt_2 = content["result"][0]["data"]["device"]["sn"]
+
+print(sn_fgt_2)
 
 ##############################################################
 # 3
@@ -142,6 +152,72 @@ headers = {
     'Content-Type': "application/json",
     'cache-control': "no-cache"
     }
+
+response = requests.request("POST", url, data=json.dumps(payload), headers=headers, verify=False)
+
+# TODO: exit if not on succesful login
+
+content = json.loads(response.content)
+sn_fgt_dc = content["result"][0]["data"]["device"]["sn"]
+
+print(sn_fgt_dc)
+
+
+#DEBUG
+sn_fgt_1 = FGVM08TM19003292
+sn_fgt_2
+sn_fgt_dc
+##############################################################
+# 3
+##############################################################
+
+payload = {
+    "session": "{{session}}",
+    "id": 1,
+    "method": "exec",
+    "params": [
+        {
+            "url": "/dvm/cmd/add/dev-list",
+            "data": {
+                "adom": "{{adom}}",
+                "flags": ["create_task", "nonblocking"],
+                "add-dev-list": [
+                    {
+                        "adm_pass": passwd,
+                        "adm_usr": user,
+                        "ip": "192.168.0.1",
+                        "name": "FGT-1",
+                        "sn": sn_fgt_1,
+                        "mgmt_mode": "fmgfaz",
+                        "flags": 24
+                    },
+                    {
+                        "adm_pass": passwd,
+                        "adm_usr": user,
+                        "ip": "192.168.0.12",
+                        "name": "FGT-2",
+                        "sn": sn_fgt_2,
+                        "mgmt_mode": "fmgfaz",
+                        "flags": 24
+                    },
+                    {
+                        "adm_pass": passwd,
+                        "adm_usr": user,
+                        "ip": "192.168.0.5",
+                        "name": "FGT-DC-5",
+                        "sn": sn_fgt_dc,
+                        "mgmt_mode": "fmgfaz",
+                        "flags": 24
+                    }
+                ]
+            }
+        }
+    ]
+}
+headers = {
+    'Content-Type': "application/json",
+    'cache-control': "no-cache"
+}
 
 response = requests.request("POST", url, data=json.dumps(payload), headers=headers, verify=False)
 
