0315788c5073e5fb181b90a4d8b8d9445e1aa969
Dmitry Perets <dperets@fortinet.com>
2024-12-24 12:03:54 +0100

    update deployment scripts

--- hol/scripts/DeploymentGuide_autodeploy.sh
+++ hol/scripts/DeploymentGuide_autodeploy.sh
@@ -2,18 +2,18 @@
 
 start=`date +%s`
 
-echo ---------------------------------------------------------------------------
-echo Downloading the latest version of the Jinja Orchestrator for release/7.4...
-echo ---------------------------------------------------------------------------
+echo -----------------------------------------------------------------------
+echo Downloading the latest version of the Jinja Orchestrator for hol-3.0...
+echo -----------------------------------------------------------------------
 mkdir -p tenants/shared
-wget -O tenants/shared/7.4.zip https://github.com/fortinet-solutions-cse/sdwan-advpn-reference/archive/refs/tags/hol-2.0.zip
-unzip -o tenants/shared/7.4.zip 'sdwan-advpn-reference-hol-2.0/dynamic-bgp-on-lo/*.j2' -d tenants/shared/
+wget -O tenants/shared/jinja-hol-3_0.zip https://github.com/fortinet-solutions-cse/sdwan-advpn-reference/archive/refs/tags/hol-3.0.zip
+unzip -o tenants/shared/jinja-hol-3_0.zip 'sdwan-advpn-reference-hol-3.0/dynamic-bgp-on-lo/*.j2' -d tenants/shared/
 
 echo
 echo ----------------------------------------------------------------------------------------
 echo Downloading the latest version of the Postman collection for the Deployment Guide 7.4...
 echo ----------------------------------------------------------------------------------------
-wget -O tenants/shared/Deployment_Guide_SDWAN_7_4_x.postman.json https://raw.githubusercontent.com/fortinet-solutions-cse/postman_collections/refs/tags/hol-2.0/Deployment_Guide_SDWAN_7_4_x.postman.json
+wget -O tenants/shared/Deployment_Guide_SDWAN_7_4_x.postman.json https://raw.githubusercontent.com/fortinet-solutions-cse/postman_collections/refs/tags/hol-3.0/Deployment_Guide_SDWAN_7_4_x.postman.json
 
 echo
 echo ------------------------------
--- hol/scripts/lab_foundation.sh
+++ hol/scripts/lab_foundation.sh
@@ -2,23 +2,23 @@
 
 start=`date +%s`
 
-echo ---------------------------------------------------------------------------
-echo Downloading the latest version of the Jinja Orchestrator for release/7.4...
-echo ---------------------------------------------------------------------------
+echo -----------------------------------------------------------------------
+echo Downloading the latest version of the Jinja Orchestrator for hol-3.0...
+echo -----------------------------------------------------------------------
 mkdir -p tenants/shared
-wget -O tenants/shared/7.4.zip https://github.com/fortinet-solutions-cse/sdwan-advpn-reference/archive/refs/tags/hol-2.0.zip
-unzip -o tenants/shared/7.4.zip 'sdwan-advpn-reference-hol-2.0/dynamic-bgp-on-lo/*.j2' -d tenants/shared/
+wget -O tenants/shared/jinja-hol-3_0.zip https://github.com/fortinet-solutions-cse/sdwan-advpn-reference/archive/refs/tags/hol-3.0.zip
+unzip -o tenants/shared/jinja-hol-3_0.zip 'sdwan-advpn-reference-hol-3.0/dynamic-bgp-on-lo/*.j2' -d tenants/shared/
 
 echo
-echo -----------------------------------------------------------------------------
-echo Downloading the latest version of the Postman collection for CSE-SDWAN-74x...
-echo -----------------------------------------------------------------------------
-wget -O tenants/shared/Managed_SDWAN_7_4_x.postman.json https://raw.githubusercontent.com/fortinet-solutions-cse/postman_collections/refs/tags/hol-2.0/Managed_SDWAN_7_4_x.postman.json
+echo -----------------------------------------------------------------------
+echo Downloading the latest version of the Postman collection for hol-3.0...
+echo -----------------------------------------------------------------------
+wget -O tenants/shared/Managed_SDWAN_7_4_x.postman.json https://raw.githubusercontent.com/fortinet-solutions-cse/postman_collections/refs/tags/hol-3.0/Managed_SDWAN_7_4_x.postman.json
 
 echo
-echo ---------------------------------------
-echo Starting the Fancy Solution Deployer...
-echo ---------------------------------------
+echo ---------------------------------
+echo Starting the Solution Deployer...
+echo ---------------------------------
 ORCH_TENANT=CustomerU ./autodeploy.py --tags foundation,lab
 
 end=`date +%s`
