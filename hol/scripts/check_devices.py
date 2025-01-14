2fe4be2547243b7a66e178e2bafb9bcfb1e9faf1
Dmitry Perets <dperets@fortinet.com>
2025-01-14 21:52:23 +0100

    add support for sdwan_only instance

--- hol/scripts/check_devices.py
+++ hol/scripts/check_devices.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
 # check_devices.py                                                           #
-# Uni-SASE HoL, Version 3.0 b100                                             #
+# Uni-SASE HoL, Version 4.0 b100                                             #
 # -------------------------------------------------------------------------- #
 # Maintainers: CSE Telco/MSSP EMEA, Fortinet                                 #
 # -------------------------------------------------------------------------- #
 
+import os
 from orch_base import runCLICommandTask, getSystemStatus
 from yaml import safe_load
 
@@ -15,25 +16,27 @@ def main():
         cfg = safe_load(cfgfile)
 
     fail = 0
-    for d in cfg['sites'].keys():
-        print()
-        print(f"--> {d}")
-        task = {
-            'site': d,
-            'cli': 'get system status'
-        }
-        try:
-            devStatus = getSystemStatus(
-                runCLICommandTask(cfg, task)
-            )
-            print(f"Version: {devStatus['Version']}")
-            print(f"Serial-Number: {devStatus['Serial-Number']}")
-            print(f"License Status: {devStatus['License Status']}")
-            if devStatus['License Status'] != "Valid": fail+=1
+    sdwan_only = os.environ.get("HOL_SDWAN")
+    for d, v in cfg['sites'].items():
+        if not sdwan_only or not v.get('sase_only'):
+            print()
+            print(f"--> {d}")
+            task = {
+                'site': d,
+                'cli': 'get system status'
+            }
+            try:
+                devStatus = getSystemStatus(
+                    runCLICommandTask(cfg, task)
+                )
+                print(f"Version: {devStatus['Version']}")
+                print(f"Serial-Number: {devStatus['Serial-Number']}")
+                print(f"License Status: {devStatus['License Status']}")
+                if devStatus['License Status'] != "Valid": fail+=1
 
-        except Exception as e:
-            print(f"\033[91m\033[1mFAILED:\033[0m {e}") 
-            fail += 1
+            except Exception as e:
+                print(f"\033[91m\033[1mFAILED:\033[0m {e}") 
+                fail += 1
     
     if (fail): 
         print("\n\033[91m\033[1mWARNING:\033[0m At least some of the devices are not in a healthy state!") 
--- hol/scripts/configure_instance.sh
+++ hol/scripts/configure_instance.sh
@@ -1,29 +1,32 @@
 #!/bin/bash
 
 # configure_instance.sh                                                      #
-# Uni-SASE HoL, Version 3.0 b100                                             #
+# Uni-SASE HoL, Version 4.0 b100                                             #
 # -------------------------------------------------------------------------- #
 # Maintainers: CSE Telco/MSSP EMEA, Fortinet                                 #
 # -------------------------------------------------------------------------- #
 
 status_fail=0
 
-# Configure external NAT
-./configure_ext_nat.py || status_fail=1
+if [[ $HOL_SDWAN -ne 1 ]]
+then
+    # Configure external NAT
+    ./configure_ext_nat.py || status_fail=1
+
+    # Update public IPs in Postman variables (CustomerU)
+    retries=5 
+    until ./update_postman_vars.py || [ $retries -le 0 ]
+    do
+        echo -e "\n\033[0;33mWARNING:\033[0m No Internet access for the lab FGTs yet (retries left = $retries)"
+        sleep 5
+        ((retries--))
+    done
+
+    [ $retries -gt 0 ] || status_fail=1 
+fi
 
 # Finalize FGT configuration
 ./configure_devices.py || status_fail=1
-
-# Update public IPs in Postman variables (CustomerU)
-retries=5 
-until ./update_postman_vars.py || [ $retries -le 0 ]
-do
-    echo -e "\n\033[0;33mWARNING:\033[0m No Internet access for the lab FGTs yet (retries left = $retries)"
-    sleep 5
-    ((retries--))
-done
-
-[ $retries -gt 0 ] || status_fail=1 
 
 exit $status_fail
 
