bd673b0e4dedb307f76b29d7a74877e93482e098
Dmitry Perets <dperets@fortinet.com>
2025-05-15 19:48:40 +0200

    add destroy_deployment script

---
+++ hol/scripts/destroy_deployment.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+# destroy_deployment.py                                                      #
+# Uni-SASE HoL, Version 4.0 b100                                             #
+# -------------------------------------------------------------------------- #
+# Maintainers: CSE Telco/MSSP EMEA, Fortinet                                 #
+# -------------------------------------------------------------------------- #
+
+from orch_base import readConfig, getApiSession
+
+
+def main():
+
+   # Config from config.yaml
+   cfg = readConfig()
+   session = getApiSession(cfg)
+
+   print("Destroying deployment...")
+
+   # Destroy tenant ADOM + any extra ADOMs it uses, as per config
+   todo = cfg.get('extra_adom', []) + [cfg['fmg_adom']]
+   for adom in todo:
+      session.deleteDevices(
+         session.getDevices(adom=adom),
+         adom=adom
+      )
+      session.deleteAdom(adom=adom)   
+
+
+if __name__ == "__main__":
+    main()   
