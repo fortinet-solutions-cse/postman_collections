16b77d26bfd85405c94996aa3e2b83aee92e6b15
Dmitry Perets <dperets@fortinet.com>
2025-05-15 14:27:07 +0200

    update shell scripts

---
+++ hol/scripts/CustomerB_destroy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# CustomerB_destroy.sh                                                       #
+# Uni-SASE HoL, Version 4.0 b100                                             #
+# -------------------------------------------------------------------------- #
+# Maintainers: CSE Telco/MSSP EMEA, Fortinet                                 #
+# -------------------------------------------------------------------------- #
+
+ORCH_TENANT=CustomerB ./destroy_deployment.py
---
+++ hol/scripts/CustomerU_destroy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# CustomerU_destroy.sh                                                       #
+# Uni-SASE HoL, Version 4.0 b100                                             #
+# -------------------------------------------------------------------------- #
+# Maintainers: CSE Telco/MSSP EMEA, Fortinet                                 #
+# -------------------------------------------------------------------------- #
+
+# $@ : optional Deployer args (e.g. '--verbose')
+
+ORCH_TENANT=CustomerU ./destroy_deployment.py
