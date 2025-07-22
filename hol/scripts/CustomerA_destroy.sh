686f32290457c77f8eb4b88981b76599bcf58487
Dmitry Perets <dperets@fortinet.com>
2025-07-22 17:31:37 +0200

    fix typo

--- hol/scripts/CustomerA_destroy.sh
+++ hol/scripts/CustomerA_destroy.sh
@@ -13,4 +13,4 @@ fgt_password=$(grep 'fgt_password:' tenants/shared/.secrets.yaml | awk '{print $
 sshpass -p $fgt_password scp -O /fortipoc/backup/site2-H1 FGT2H1:fgt-restore-config
 
 echo Please wait for the site2-H1 to reboot before starting your next deployment!
-echo It will take around a minute (use 'fpoc_ver' to check)!
+echo It will take around a minute (use fpoc_ver to check)!
