c08e5529c52c9b9d366c3d06a7307e7d7b280007
Dmitry Perets <dperets@fortinet.com>
2025-11-17 12:11:38 +0100

    refactor CustomerA deployment

--- hol/scripts/CustomerU_autodeploy_west.sh
+++ hol/scripts/CustomerU_autodeploy_west.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# CustomerU_autodeploy_all.sh                                                #
+# CustomerU_autodeploy_west.sh                                                #
 # Uni-SASE HoL, Version 4.0 b100                                             #
 # -------------------------------------------------------------------------- #
 # Maintainers: CSE Telco/MSSP EMEA, Fortinet                                 #
