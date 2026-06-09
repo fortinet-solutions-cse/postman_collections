056a5eadd7ee85928f526c8fa0bf7b79b4787193
Dmitry Perets <dperets@fortinet.com>
2026-06-09 16:52:29 +0200

    refactor fine-tune-sase tag

--- hol/scripts/CustomerU_autodeploy_west.sh
+++ hol/scripts/CustomerU_autodeploy_west.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # CustomerU_autodeploy_west.sh                                               #
-# Uni-SASE HoL, Version 4.5 b451                                             #
+# Uni-SASE HoL, Version 4.5 b452                                             #
 # -------------------------------------------------------------------------- #
 # Maintainers: CSE Telco/MSSP EMEA, Fortinet                                 #
 # -------------------------------------------------------------------------- #
 
 # $@ : optional Deployer args (e.g. '--verbose')
 
-[[ $HOL_SDWAN -eq 1 ]] && skip_tags="fine-tune-sase,"
+[[ $HOL_SDWAN -eq 1 ]] && skip_tags="mixed,"
 
-ORCH_TENANT=CustomerU ./hol_autodeploy.sh --skip-tags ${skip_tags}west-east,bor "$@"
+ORCH_TENANT=CustomerU ./hol_autodeploy.sh --skip-tags ${skip_tags}fine-tune-sase,west-east,bor "$@"
