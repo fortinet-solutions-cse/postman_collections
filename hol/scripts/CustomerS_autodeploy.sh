36fc9a1b0d23da3d0572583a2249efcf93c7c2f1
Praveena Shanubhogue <pshanubhogue@fortinet.com>
2026-09-22 11:08:43 +0200

    CustomerS for SovSASE

---
+++ hol/scripts/CustomerS_autodeploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# CustomerS_autodeploy_west.sh                                               #
+# Uni-SASE HoL, Version 4.5 b452                                             #
+# -------------------------------------------------------------------------- #
+# Maintainers: CSE Telco/MSSP EMEA, Fortinet                                 #
+# -------------------------------------------------------------------------- #
+
+# $@ : optional Deployer args (e.g. '--verbose')
+
+[[ $HOL_SDWAN -eq 1 ]]
+
+ORCH_TENANT=CustomerS ./hol_autodeploy.sh "$@"
